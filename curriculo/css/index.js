3c34690df74eb26eca1b5ca89a7286b34f9b3f2b
Robert <robprodrigues@gmail.com>
2021-02-07 23:30:41 -0300

    revisão

---
+++ curriculo/css/index.js
@@ -0,0 +1,35 @@
+var nome = "Robert Rodrigues";
+var cargo = "Desenvolvedor";
+
+var nomehtml = document.getElementById("nome-no-html");
+var cargohtml = document.getElementById("cargo-no-html");
+var texto1 = document.getElementById("texto-1");
+var texto2 = document.getElementById("texto-2");
+
+function colocarnomenohtml(nome) {
+nomehtml.innerHTML = nome;
+}
+
+function colocarcargonohtml(cargo) {
+    cargohtml.innerHTML = cargo;
+}
+
+function logarnome() {
+    console.log(nome);
+}
+
+function clicknoProjetos() {
+    console.log("clicou no botão Projetos");
+    texto2.style.display = "block";
+    texto1.style.display = "none";
+}
+
+function clicknoSobre() {
+
+    console.log("clicou no botão Sobre");
+    texto2.style.display = "none";
+    texto1.style.display = "block";
+}
+
+colocarnomenohtml(nome);
+colocarcargonohtml(cargo);
